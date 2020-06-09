c665f45f512ec40af83ba5294956efdcc10b7670
Alvaro Sebastian Mañuico Quequejana <alvaro.sebastian98@gmail.com>
2020-06-08 20:44:54 -0500

    first commit

---
+++ CodeFirstUniversity/Models/OfficeAssignment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstUniversity.Models
+{
+    public class OfficeAssignment
+    {
+        public OfficeAssignment()
+        {
+            this.Activo = true;
+        }
+
+        [Key]
+        [ForeignKey("Instructor")]
+        public int InstructorID { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Office Location")]
+        public string Location { get; set; }
+
+        public virtual Instructor Instructor { get; set; }
+
+        public bool Activo { get; set; }
+    }
+}
---
+++ CodeFirstUniversity/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstUniversity.Models
+{
+    public class Student : Person
+    {
+        public Student()
+        {
+            this.Activo = true;
+        }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+        public bool Activo { get; set; }
+    }
+}
